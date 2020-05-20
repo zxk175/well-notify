e99109ba2d1213dfc2264c3a1b3210b9af574b0a
zxk175 <zxk175@qq.com>
2020-05-20 22:13:39 +0800

    initial commit

--- notify-admin/src/main/java/com/zxk175/notify/module/bean/param/notify/NotifyMsgListParam.java
+++ notify-admin/src/main/java/com/zxk175/notify/module/bean/param/notify/NotifyMsgListParam.java
@@ -12,14 +12,17 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class NotifyMsgListParam extends PageParam {
-	
-	@ApiModelProperty(value = "标题", example = "异常")
-	private String title;
-	
-	@ApiModelProperty(value = "创建时间", example = "2020-05-09")
-	private String createTime;
-	
-	@ApiModelProperty(value = "通道Id", example = "1174160779906125825")
-	private String channelId;
-	
+
+    @ApiModelProperty(value = "标题", example = "异常")
+    private String title;
+
+    @ApiModelProperty(value = "是否发送 0-未发 1-已发", example = "1")
+    private Integer sendFlag;
+
+    @ApiModelProperty(value = "通道Id", example = "1174160779906125825")
+    private String channelId;
+
+    @ApiModelProperty(value = "创建时间", example = "2020-05-09")
+    private String createTime;
+
 }
