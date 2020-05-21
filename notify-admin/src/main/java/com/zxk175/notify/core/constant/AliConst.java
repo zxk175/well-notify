1b2d1b449333556ec09a57356fe58cae65e4a9bf
zxk175 <zxk175@qq.com>
2020-05-21 18:02:31 +0800

    initial commit

--- notify-admin/src/main/java/com/zxk175/notify/core/constant/AliConst.java
+++ notify-admin/src/main/java/com/zxk175/notify/core/constant/AliConst.java
@@ -9,9 +9,9 @@ public class AliConst {
     public static final String ALI_KEY = "LTAIAL87GqfuN1kg";
     public static final String ALI_SECRET = "v6UwUE7DVXwzPHve47Bs0iSiTqRGFo";
 
-    public static final String BASE_URL = "http://res.zxk175.com/";
+    public static final String BASE_URL = "https://cdn.zxk175.com/";
 
-    public static final String BUCKET_NAME = "well-back";
+    public static final String BUCKET_NAME = "well-data";
 
     /**
      * 外网：https://oss-cn-shenzhen.aliyuncs.com
